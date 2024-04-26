8d0bb1c90f0ba58cb919ebb92c2af5c66bb2e57d
MuhammadAizazA <aizaz.ali.shah.0205@gmail.com>
2024-04-26 05:46:29 +0000

    small changes

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Site is Working\nApi's You can Call\nmake_transactions\nread_transactions_from_mongodb\n"}
+    return {"message": "Site is Working---Api's--- You can Call---\make_transactions---\read_transactions_from_mongodb"}
 
 @app.get("/make_transactions")
 async def transaction_maker():
